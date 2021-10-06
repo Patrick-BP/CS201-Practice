d8470e7eda344c7c135091d2b568827dcfc5693e
Patrick <papybp@gmail.com>
2021-10-05 22:33:17 -0500

    5 oct

---
+++ src/chapter4/Prime.java
@@ -0,0 +1,34 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class Prime {
+
+    public static void main(String[] args) {
+        isPrime();
+
+    }
+    public static void isPrime(){
+        Scanner key = new Scanner(System.in);
+        int n;
+        do {
+            System.out.println("Enter a number");
+            n = key.nextInt();
+
+            if(!(n>=1)) break;
+
+            int count = 0;
+            for (int i = 1; i <= 100; i++) {
+                if (n % i == 0) {
+                    count++;
+                }
+            }
+            if (count > 2) {
+                System.out.println("not a prime");
+            } else {
+                System.out.println("prime");
+            }
+        }while(n>0);
+
+    }
+}
